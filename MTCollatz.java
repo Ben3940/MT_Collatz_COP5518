4cc728fce3474fd6d75927ee68a4ae2464685b74
Ben3940 <bty2015@comcast.net>
2023-07-06 19:16:35 -0400

    Added initial layout for MTCollatz class and created a class specifically for the shared array between threads

--- MTCollatz.java
+++ MTCollatz.java
@@ -1,1 +1,29 @@
+package MT_Collatz_COP5518;
 
+import java.time.Instant;
+
+import MT_Collatz_COP5518.SharedArray;
+
+
+public class MTCollatz extends Thread {
+
+    private String _threadID;
+    private SharedArray _sharedArray;
+
+
+    public MTCollatz(String threadID, SharedArray sharedArray){
+        this._threadID = threadID;
+        this._sharedArray = sharedArray;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Thread created");
+    }
+
+    public static void main(String[] args){
+        // Instance of sharedArray that will be referenced by all MTCollatz objects created
+        SharedArray sharedArray = new SharedArray();
+
+    }
+}
